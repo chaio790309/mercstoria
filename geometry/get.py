16524bf8cdcf71769b8f07bfdb2f9b30e665ec92
Milkshake <chaio790309@gmail.com>
2022-02-19 22:44:05 +0800

    match

--- geometry/get.py
+++ geometry/get.py
@@ -18,7 +18,18 @@ def get(x):
     #print(ass)
     re=[]
     for a in ass:
-        if n in [5,6,8,9,10,16,22,23,24]: #1.國別 2.年齡 3.性別 4.星數 5.屬性 6.成長類型 7.持有武器 8.武器類型 9.體數 10.段數 11.初期HP 12.滿HP 13.滿覺HP 14.進化HP 15移速
+        if n == 6:
+            t=0
+            match a.text[len(a.span.string)+1:].replace("\n",""):
+                case "早熟":
+                    t="12%"
+                case "平均":
+                    t="8%"
+                case "晩成":
+                    t="6%"
+            re=re+[t]
+            n+=1
+        elif n in [5,6,8,9,10,22,23,24]: #1.國別 2.年齡 3.性別 4.星數 5.屬性 6.成長類型 7.持有武器 8.武器類型 9.體數 10.段數 11.初期HP 12.滿HP 13.滿覺HP 14.進化HP 15移速
                                     #16.手長 17.DPS 18.覺醒DPS 19.進化DPS 20.初期ATK 21.滿ATK 22.滿覺ATK 23.進化ATK 24.攻擊間隔 25.韌性 26.綜合DPS 27覺醒綜合DPS 28.進化綜合DPS
             # print(a)
             # print(a.span)
@@ -28,19 +39,22 @@ def get(x):
             n+=1
         else:
             n+=1
-    if re[7]>re[6]: #pop移除特定list位置
-        re.pop(6)
+    if re[6]>re[5]: #pop移除特定list位置 判定是否有進化 並在文字前附加[S]
+        re.pop(5)
+        name="[S]"+name.string
     else:
-        re.pop(7)
+        re.pop(6)
+        name=name.string
     if re[4] == "":
         re[4]="1"
 
-    ele=root.find("div",class_="db_other_text")
-    ele=(ele.text.replace("\n\xa0\n","")).replace("\n","").replace("炎","").replace("水","").replace("風","").replace("光","").replace("闇","")
+    element=root.find("div",class_="db_other_text")
+    element=(element.text.replace("\n\xa0\n","")).replace("\n","").replace("炎","").replace("水","").replace("風","").replace("光","").replace("闇","").replace("％","%")
     #print(ele)
-    ele=ele.split("属性")
+    element=element.split("属性")
     #print(ele[1:])
 
 
-    re=[name.string]+re+ele[1:]
-    print(re)
+    #re=[name]+re+ele[1:]
+    #print([name]+re+ele[1:])
+    return [name]+re+element[1:]
--- unit.py
+++ unit.py
@@ -15,26 +15,26 @@ ok=False #篩選
 n="" #紀錄文字
 b=0 #後衛數量
 import geometry.get as g #寫好的函數
-import urllib.parse
+import urllib.parse #將文字轉碼給url用
 for name in names:
     if name.span != None: # 一個傭兵含12個<td> 其中10個class="no-min-width" 第一個不含span 0.名稱 1.武器 2.體數 3.段數 4.手長 
-        if a == 0:
-            n=urllib.parse.quote(name.span.string) #先取得文字
-            a+=1
-            s+=1
-        elif a == 1:
-            if name.span.string in ["弓矢","魔法","銃弾"]:
-                ok=True
-            a+=1
-        elif a == 4:
-            if (int(name.span.string) >150 and ok == True ):
-                g.get("https://xn--cckza4aydug8bd3l.gamerch.com/"+n)
-                b=b+1
-            a+=1
-        elif a < 8:
-            a+=1
-        else:
-            a=0
-            ok=False
-print("總共",s,"隻5星傭兵")
-print("其中有",b,"個後衛")
+        match a%9:
+            case 0:
+                n=urllib.parse.quote(name.span.string) #取得文字
+                a+=1
+                s+=1
+            case 1:
+                if name.span.string in ["弓矢","魔法","銃弾"]: #確認條件
+                    ok=True
+                a+=1
+            case 4:
+                if (int(name.span.string) >150 and ok == True ): #確認條件2
+                    result=g.get("https://xn--cckza4aydug8bd3l.gamerch.com/"+n)
+                    print(result)
+                    b=b+1
+                a+=1
+                ok=False
+            case _:
+                a+=1
+print("總共",s,"個5星傭兵")
+print("其中有",b,"個符合條件")
